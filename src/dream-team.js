d797608bc60a7a46d88bf2139d4b669f344b0ffd
Artur Pakhomau <al9966@mail.ru>
2020-09-28 12:19:40 +0300

    fix: added task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(data) {
+  if ( !(Array.isArray(data)) ) {
+    return false;
+  }else {
+    let password=[]; 
+    data.forEach(num =>{
+      if(typeof num === 'string'){
+        let result = num.trim()
+        password.push(result[0].toUpperCase())
+      }
+    })
+    return password.sort().join("");
+  }
+
 };
